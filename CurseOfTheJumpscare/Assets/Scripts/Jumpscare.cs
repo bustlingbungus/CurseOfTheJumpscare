ce5cab4c4b5aa79666eff6c8c84a92c2f46dbc52
bustlingbungus <cadens@usf.edu>
2024-10-30 22:38:07 -0400

    audio for jumpscare

--- CurseOfTheJumpscare/Assets/Scripts/Jumpscare.cs
+++ CurseOfTheJumpscare/Assets/Scripts/Jumpscare.cs
@@ -6,11 +6,21 @@ public class Jumpscare : MonoBehaviour
     // the amount of time (in seconds) the jumpscare will exist
     public float scareTime = 5.0f;
 
+    // audio that plays on jumpscare
+    private AudioSource dracula, vine_boom;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // get sounds effects
+        var audio = GetComponents<AudioSource>();
+        // ensure all effects were found before accessing them 
+        if (audio.Length>0) {
+            dracula = audio[0]; dracula.Play(0);
+        }
+        if (audio.Length>1) {
+            vine_boom = audio[1]; vine_boom.Play(0);
+        }
     }
 
     // Update is called once per frame
